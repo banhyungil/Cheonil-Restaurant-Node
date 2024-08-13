f722d0787672617375d715c52b1df69db60d5525
banhyungil <byban4186@gmail.com>
2024-08-13 23:38:17 +0900

    feat: 주문 생성, 수정시 Menu정보 추가 반환하도록 수정

--- src/services/OrderService.ts
+++ src/services/OrderService.ts
@@ -1,12 +1,18 @@
 import DB from '@src/models'
 
-const { MyOrder, OrderMenu } = DB.Models
+const { MyOrder, OrderMenu, Menu } = DB.Models
 async function getOrder(seq: number) {
     const nMyOrder = await MyOrder.findOne({
         include: [
             {
                 model: OrderMenu,
                 as: 'orderMenues',
+                include: [
+                    {
+                        model: Menu,
+                        as: 'menu',
+                    },
+                ],
             },
         ],
         where: {
